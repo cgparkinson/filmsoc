83254485f3cb0a6c4e8ec7364d4c7812260ec079
Ben Tattersley <ubershinysheep@gmail.com>
2014-08-06 22:58:15 +0100

    Competition page

---
+++ competition/index.php
@@ -0,0 +1,79 @@
+<?php
+	require_once('../lib.php');
+?>
+<!DOCTYPE html>
+<!--[if lt IE 7]>	   <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>		   <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>		   <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<title>Bede Film Society - Sponsor Time Competition</title>
+	<meta name="description" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<link href='//fonts.googleapis.com/css?family=Open+Sans:400,300,600' rel='stylesheet' type='text/css'>
+	<link rel="shortcut icon" href="../favicon.ico">
+	<link rel="stylesheet" href="../css/main.css">
+	<script type="text/javascript"
+      src="//maps.googleapis.com/maps/api/js?sensor=false">
+    </script>
+    <script src="../js/libs/modernizr-2.6.2.min.js"></script>
+</head>
+<body id="history">
+	<!--[if lt IE 8]>
+		<p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+	<![endif]-->
+
+	
+	<header id="top">
+		<nav>
+			<a href="../">Home</a>
+		</nav>
+	</header>
+	
+	
+	<div class="hero" style="background-image: url('../images/curtain.jpg')">
+		<div class="hero-wrapper">
+			<h3>Win A Free Film</h3>
+			<div class="tape">
+				<h1>
+					<span>B</span><span>e</span><span>d</span><span>e</span><br>
+					<span>F</span><span>i</span><span>l</span><span>m</span><br>
+					<span>S</span><span>o</span><span>c</span>
+				</h1>
+			</div>
+			<p>Take part in our exciting competition</p>
+		</div>
+	</div>
+	
+	<main>
+	
+	
+	<section>
+		<h2>What's all this about?</h2>
+		<p>If you've been to one of our showings recently then you'll notice we've been showing short clips before the ads start playing. We want to encourage creativity and student film making so now is your chance to get noticed and have your work shown on our big screen!</p>
+		
+		<h2>Great! How can I enter?</h2>
+		<p>All we need is a short video from you! Make us laugh, cry or awestruck! Here's a little step by step guide on what to do:</p>
+		<ol>
+			<li>Create a short video 10 seconds or more in duration. Don't worry about adding the sponsor time banner, we'll do that bit.</li>
+			<li>Upload it somewhere on the web. YouTube, Vimeo, Dropbox are all okay to use.</li>
+			<li>Tweet us the link <a href="http://twitter.com/bedefilmsoc">@BedeFilmSoc</a>, <a href="https://www.facebook.com/messages/251043086386">message us on Facebook</a> or send an <a href="mailto:hildbede.filmsociety@durham.ac.uk?Subject=Competition%20Entry">email</a>.</li>
+		</ol>
+		
+		<p>If we like what you've done then we'll get in touch and let you know when and how to claim your free film.</p>
+		
+		<h2>Are there any rules?</h2>
+		<p>To have your work shown and win free entry to a film for you and one friend all you have to do is submit an appropriate clip. Try not to swear or do anything too rude! We've got to make sure the clips are appropriate for the film. We reserve the right to reject any entries that might be inappropriate. We're also not responsible for any loss or damages to you or anything because of an activity associated with this competition, please don't do anything dangerous!</p>
+		
+		
+	</section>
+		
+	</main>
+	
+<?php
+	include('../footer.php')
+?>
+</body>
+</html>
